a40f652ce5e82bcf7e4e150250f6bd52d9b2e215
Matt Hamer <matt@attribyte.com>
2018-06-13 10:27:03 -0400

    Add file-based "db" for post meta. Add support to save a post fingerprint with post meta.

--- src/main/java/com/attribyte/relay/wp/PostMeta.java
+++ src/main/java/com/attribyte/relay/wp/PostMeta.java
@@ -14,14 +14,23 @@
 
 package com.attribyte.relay.wp;
 
+import com.google.common.base.Charsets;
 import com.google.common.base.MoreObjects;
+import com.google.common.base.Strings;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
 import com.google.common.primitives.Longs;
 import com.google.protobuf.ByteString;
+import org.attribyte.wp.model.Meta;
+import org.attribyte.wp.model.Post;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.ISODateTimeFormat;
 
 import java.io.IOException;
+import java.util.Comparator;
 
 /**
  * Post metadata (id, timestamp) for serializing state.
@@ -34,13 +43,34 @@ public class PostMeta {
    public static final PostMeta ZERO = new PostMeta(0L, 0L);
 
    /**
-    * Creates post metadata.
+    * Creates post metadata with no fingerprint.
     * @param id The post id.
     * @param lastModifiedMillis The last modified timestamp.
     */
    public PostMeta(final long id, final long lastModifiedMillis) {
       this.id = id;
       this.lastModifiedMillis = lastModifiedMillis;
+      this.fingerprint = null;
+   }
+
+   /**
+    * Creates post metadata with a fingerprint.
+    * @param id The post id.
+    * @param lastModifiedMillis The last modified timestamp.
+    * @param fingerprint The fingerprint.
+    */
+   public PostMeta(final long id, final long lastModifiedMillis, final HashCode fingerprint) {
+      this.id = id;
+      this.lastModifiedMillis = lastModifiedMillis;
+      this.fingerprint = fingerprint;
+   }
+
+   /**
+    * Creates post metadata with a fingerprint from a post.
+    * @param post The post.
+    */
+   public PostMeta(final Post post) {
+      this(post.id, post.modifiedTimestamp, fingerprint(post));
    }
 
    /**
@@ -53,13 +83,19 @@ public class PostMeta {
     */
    public final long lastModifiedMillis;
 
+   /**
+    * A hash 'fingerprint' for detecting changes.
+    */
+   public final HashCode fingerprint;
+
    @Override
    public String toString() {
       return MoreObjects.toStringHelper(this.getClass())
               .add("id", id)
               .add("lastModifiedMillis",lastModifiedMillis)
-              .add("lastModifiedHumanUTC", ISODateTimeFormat.basicDateTime().withZoneUTC().print(lastModifiedMillis))
               .add("lastModifiedHuman", ISODateTimeFormat.basicDateTime().withZone(DateTimeZone.getDefault()).print(lastModifiedMillis))
+              .add("lastModifiedHumanUTC", ISODateTimeFormat.basicDateTime().withZoneUTC().print(lastModifiedMillis))
+              .add("fingerprint", fingerprint)
               .toString();
    }
 
@@ -68,7 +104,13 @@ public class PostMeta {
     * @return The byte string.
     */
    public ByteString toBytes() {
-      return ByteString.copyFromUtf8(id + "," + lastModifiedMillis);
+      if(fingerprint == null) {
+         return ByteString.copyFromUtf8(id + "," + lastModifiedMillis);
+      } else {
+         return ByteString.copyFromUtf8(id + "," + lastModifiedMillis + "," + fingerprint.toString() + "," +
+                 ISODateTimeFormat.basicDateTime().withZoneUTC().print(lastModifiedMillis)
+         );
+      }
    }
 
    /**
@@ -94,6 +136,58 @@ public class PostMeta {
          throw new IOException(String.format("Invalid state: '%s'", bytes.toStringUtf8()));
       }
 
-      return new PostMeta(id, lastModifiedMillis);
+      if(components.length >= 3) {
+         return new PostMeta(id, lastModifiedMillis, HashCode.fromString(components[2].trim()));
+      } else {
+         return new PostMeta(id, lastModifiedMillis);
+      }
    }
+
+   /**
+    * Creates a unique fingerprint for a post using the default hasher.
+    * @param post The post.
+    * @return The fingerprint.
+    */
+   public static final HashCode fingerprint(final Post post) {
+      return fingerprint(post, hashFunction.newHasher());
+   }
+
+   /**
+    * Creates a unique fingerprint for a post.
+    * @param post The post.
+    * @param hasher A hasher.
+    * @return The fingerprint.
+    */
+   public static final HashCode fingerprint(final Post post, final Hasher hasher) {
+      if(post == null) {
+         return null;
+      }
+
+      hasher.putString(Strings.nullToEmpty(post.slug), Charsets.UTF_8);
+      hasher.putString(Strings.nullToEmpty(post.title), Charsets.UTF_8);
+      hasher.putString(Strings.nullToEmpty(post.excerpt), Charsets.UTF_8);
+      hasher.putString(Strings.nullToEmpty(post.content), Charsets.UTF_8);
+      hasher.putLong(post.authorId);
+      hasher.putLong(post.publishTimestamp);
+      hasher.putString(post.status != null ? post.status.toString() : "", Charsets.UTF_8);
+      hasher.putString(Strings.nullToEmpty(post.guid), Charsets.UTF_8);
+      post.metadata.stream().sorted(Comparator.comparing(o -> o.key)).forEach(meta -> {
+         hasher.putString(meta.key, Charsets.UTF_8).putString(Strings.nullToEmpty(meta.value), Charsets.UTF_8);
+      });
+      hasher.putString(post.type != null ? post.type.toString() : "", Charsets.UTF_8);
+      hasher.putString(Strings.nullToEmpty(post.mimeType), Charsets.UTF_8);
+      post.tags().stream().sorted(Comparator.comparing(o -> o.term.name)).forEach(term -> {
+         hasher.putString(term.term.name, Charsets.UTF_8);
+      });
+      post.categories().stream().sorted(Comparator.comparing(o -> o.term.name)).forEach(term -> {
+         hasher.putString(term.term.name, Charsets.UTF_8);
+      });
+      post.children.forEach(childPost -> fingerprint(childPost, hasher));
+      return hasher.hash();
+   }
+
+   /**
+    * The hash function.
+    */
+   public static final HashFunction hashFunction = Hashing.sha256();
 }
---
+++ src/main/java/com/attribyte/relay/wp/PostMetaDB.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2018 Attribyte, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package com.attribyte.relay.wp;
+
+import com.google.common.io.Files;
+import com.google.protobuf.ByteString;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PostMetaDB {
+
+   /**
+    * Creates the DB.
+    * @param dir The directory for files.
+    */
+   public PostMetaDB(final File dir) {
+      this.dir = dir;
+   }
+
+   /**
+    * Writes post meta.
+    * @param meta The meta to write.
+    * @return The input metadata.
+    * @throws IOException on write error.
+    */
+   public final PostMeta write(final PostMeta meta) throws IOException {
+      File file = file(meta.id);
+      Files.write(meta.toBytes().toByteArray(), file);
+      return meta;
+   }
+
+   /**
+    * Reads post meta for an id.
+    * @param id The id.
+    * @return The post meta or {@code null} if not found.
+    * @throws IOException on read error.
+    */
+   public final PostMeta read(final long id) throws IOException {
+      File file = file(id);
+      if(file.exists() && file.canRead()) {
+         return PostMeta.fromBytes(ByteString.copyFrom(Files.toByteArray(file)));
+      } else {
+         return null;
+      }
+   }
+
+   /**
+    * Creates the file for storage with a subdirectory based on
+    * the last two digits of the id.
+    * @param id The id.
+    * @return The file.
+    */
+   public final File file(final long id) {
+      String idStr = Long.toString(id);
+      final String subdir;
+      if(idStr.length() > 1) {
+         subdir = idStr.substring(idStr.length() - 2);
+      } else {
+         subdir = "0" + idStr;
+      }
+      return new File(new File(dir, subdir), idStr);
+   }
+
+   /**
+    * The directory for files.
+    */
+   private final File dir;
+}
